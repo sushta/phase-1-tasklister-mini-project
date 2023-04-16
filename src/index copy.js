03bcefd93f0ba400185e9aa56a0f7ff83e230972
sustapakhreen <sushta48@gmail.com>
2023-04-16 13:50:19 -0500

    adds sorting and delete

--- src/index copy.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let form = document.querySelector("form")
-  form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    createToDOList(e.target['new-task-description'].value)
-    form.reset()
-  })
-});
-
