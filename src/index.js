72210c54f50e863eae93e2296210d376fcc5f140
sustapakhreen <sushta48@gmail.com>
2023-04-15 22:33:52 -0500

    TaskLister Mini Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createToDOList(e.target['new-task-description'].value)
+    form.reset()
+  })
 });
+
